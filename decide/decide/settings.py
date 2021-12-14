5db7716714465ca4c6be18eb150b1aadb3a9f528
danricost <danricost@alum.us.es>
2021-12-14 14:24:31 +0100

    feat:Añadir configuracion de la verificacion por correo del registro de usuarios #14
    Se ha establecido la configuracion de la verificacion, asi como el texto a mostrar en el correo.
    
    Closes #14

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -180,3 +180,13 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'decide.veleta.g4@gmail.com'
+EMAIL_HOST_PASSWORD = 'DavidBenavides'
