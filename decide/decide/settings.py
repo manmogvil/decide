130b6b79c446d3908866fd314427cdf61ae3467f
manmogvil <manmogvil@gmail.com>
2021-12-16 17:19:52 +0100

    Prueba de integracion continua

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -70,7 +70,7 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://ejemplo1233.herokuapp.com/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -180,3 +180,10 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+
+NOSE_ARGS = [
+    '--with-xunit'
+]
+import django_heroku
+django_heroku.settings(locals(),test_runner=False)
