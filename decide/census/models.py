c7b6ebe376cb0ee9cb18652e40d16b0e250762f2
Juanmagc99 <juanma101099@gmail.com>
2021-12-19 16:35:34 +0100

    fix:Cambiada ubicación de notificaciones
    Pasado las notificaciones a views y se ha puesto su url de forma dinámica
    
    closes #41

--- decide/census/models.py
+++ decide/census/models.py
@@ -11,18 +11,4 @@ class Census(models.Model):
         unique_together = (('voting_id', 'voter_id'),)
 
     def save(self, *args, **kwargs):
-        user_added = User.objects.filter(id=self.voter_id).values()
-        if(user_added):
-            nombre = str(user_added[0].get('first_name'))
-            email = str(user_added[0].get('email'))
-            
-            send_mail(
-            'Añadido al censo de una votación',
-            'Hola '+nombre+' acabas de ser añadido como participante en una votación, para acceder a ella entra en http://localhost:8000/booth/'+str(self.voting_id)+'/',
-            None,
-            [email],
-            fail_silently=False,
-            )
-
-
         return super().save()
